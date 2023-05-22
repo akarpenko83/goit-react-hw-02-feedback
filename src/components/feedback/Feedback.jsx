002e497cd88654614ac8768f44d0d7286c6fcc94
Anton K <akarpenko83@gmail.com>
2023-05-22 14:52:42 +0300

    Update Feedback.jsx

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 
 class Feedback extends Component {
     state = {
-        good: 3,
+        good: 7,
         neutral: 5,
-        bad: 1,
+        bad: 2,
     };
 
-    countTotalFeedback = () => {};
+    countTotalFeedback = () => {
+        return (
+            this.state.good +
+            this.state.neutral +
+            this.state.bad
+        );
+    };
 
-    countPositiveFeedbackPercentage = () => {};
+    countPositiveFeedbackPercentage = () => {
+        const total =
+            this.state.good +
+            this.state.neutral +
+            this.state.bad;
+        return (this.state.good / total).toFixed(2);
+    };
 
     render() {
         return (
@@ -22,7 +34,12 @@ class Feedback extends Component {
                 <p>Good: {this.state.good}</p>
                 <p>Neutral: {this.state.neutral}</p>
                 <p>Bad: {this.state.bad}</p>
-                <p>Positive feedback: %</p>
+                <p>Total: {this.countTotalFeedback()}</p>
+                <p>
+                    Positive feedback:{' '}
+                    {this.countPositiveFeedbackPercentage()}
+                    %
+                </p>
             </div>
         );
     }
