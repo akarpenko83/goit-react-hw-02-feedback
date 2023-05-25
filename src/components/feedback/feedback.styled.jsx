e1f32986daeb1c6050d58ec55bfaa72ccd9b709d
Anton K <akarpenko83@gmail.com>
2023-05-25 11:19:10 +0300

    add live background

--- src/components/feedback/feedback.styled.jsx
+++ src/components/feedback/feedback.styled.jsx
@@ -1,15 +1,18 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.section`
+    position: absolute;
+    z-index: 1000;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
     display: flex;
     flex-direction: column;
     width: 30vw;
-    height: 100%;
-    outline: solid 1px lightgray;
     margin: 0 auto;
     padding: 20px;
     align-items: center;
-
+    background-color: lightgray;
     box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
         0px 1px 1px 0px rgba(0, 0, 0, 0.14),
         0px 2px 1px -1px rgba(0, 0, 0, 0.12);
